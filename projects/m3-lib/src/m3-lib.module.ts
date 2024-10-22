8b360c0cc49b0039e964c9a1d15fbbde9f1f7dea
Alex Pereira Oliveira <lexpdigi@gmail.com>
2024-10-22 15:43:13 -0300

    ajuste d emodules

---
+++ projects/m3-lib/src/m3-lib.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { M3InputComponent } from './lib/m3-input/m3-input.component';
+
+@NgModule({
+  declarations: [M3InputComponent],
+  exports: [M3InputComponent] // Exporte o componente para uso externo
+})
+export class M3LibModule {}
--- projects/m3-lib/src/public-api.ts
+++ projects/m3-lib/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of m3-lib
  */
 
-export * from './lib/m3-lib.service';
-export * from './lib/m3-lib.component';
+export * from './lib/m3-input/m3-input.component'; // Exporta o componente
+export * from './m3-lib.module';
